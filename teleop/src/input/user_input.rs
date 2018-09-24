b4a99ab53ba9292b7d4b7532f245cbc8228c3cd4
Max Strange <max.strange@synapse.com>
2018-09-23 21:09:02 -0700

    Done with teleop for now

--- teleop/src/input/user_input.rs
+++ teleop/src/input/user_input.rs
@@ -1,6 +1,8 @@
 pub mod user_input {
     use commands;
+    use std::fs;
     use std::io;
+    use std::io::BufRead;
     use std::sync::mpsc;
 
     /// Reads lines from the user until the quit command is given.
@@ -49,6 +51,28 @@ pub mod user_input {
     /// Opens the given file, reads its contents, then executes each line as if it were
     /// a command entered into the console. Does not accept Quit commands or other script commands.
     fn run_script(tx: &mpsc::Sender<commands::Command>, fpath: &str) {
-        // TODO: execute each line in the script
+        let mut cmds = Vec::new();
+        match fs::File::open(fpath) {
+            Ok(file) => {
+                for (lineno, line) in io::BufReader::new(file).lines().enumerate() {
+                    // try to convert into a cmd
+                    match commands::Command::new_from_string(&line.expect(&format!("Couldn't read line {}", lineno))) {
+                        Ok(cmd) => cmds.push(cmd),
+                        Err(msg) => {
+                            println!("Problem with script at line {}: {}", lineno, msg);
+                            return;
+                        },
+                    }
+                }
+            },
+            Err(e) => {
+                println!("Could not open {}. Error: {:?}", fpath, e);
+            },
+        }
+
+        // Try to execute each command
+        for c in cmds {
+            execute_command(c, tx);
+        }
     }
 }
