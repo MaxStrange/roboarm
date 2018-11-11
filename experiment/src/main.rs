fa279f3bb05398b6794d121923c427d4e0e85d1f
Max Strange <max.strange@synapse.com>
2018-11-11 14:09:40 -0800

    WIP: Mostly done with random experiment

--- experiment/src/main.rs
+++ experiment/src/main.rs
@@ -17,18 +17,43 @@
 
 /* Externs */
 extern crate config;
+extern crate num;
+extern crate rand;
 
 /* Modules */
 mod expconfig;
 mod expresults;
 
-/* Use statements */
+/* Uses */
+use rand::prelude::*;
 use std::env;
 use std::path;
 use std::process;
 use std::collections::{hash_map, HashMap};
+use std::fmt::Write;
+use std::io::Write as _w; // have to include two different Writes
+use std::fs::File;
+
+/* Selfs */
 use self::expconfig::{Mode, ExperimentConfig};
 use self::expresults::ExperimentResults;
+
+/* Consts */
+const ANGLE_START_BASE: isize = 90;
+const ANGLE_START_SHOULDER: isize = 10;
+const ANGLE_START_ELBOW: isize = 155;
+
+const ANGLE_LOWER_LIMIT_BASE: isize = 0;
+const ANGLE_LOWER_LIMIT_SHOULDER: isize = 0;
+const ANGLE_LOWER_LIMIT_ELBOW: isize = 100;
+
+const ANGLE_UPPER_LIMIT_BASE: isize = 180;
+const ANGLE_UPPER_LIMIT_SHOULDER: isize = 50;
+const ANGLE_UPPER_LIMIT_ELBOW: isize = 180;
+
+const BASENUM: usize = 0;
+const SHOULDERNUM: usize = 1;
+const ELBOWNUM: usize = 2;
 
 fn main() {
     let usage = "Need a path to a valid configuration file.";
@@ -85,19 +110,95 @@ fn main() {
 }
 
 fn run_experiment<'a>(experiment: &'a ExperimentConfig) -> ExperimentResults<'a> {
+    // Create the random number generator
+    let mut rng = thread_rng();
+
+    // Create the results to record to
     let mut results = ExperimentResults::new(&experiment);
 
+    // Run the whole experiment (each episode)
     for episode in 0..experiment.nepisodes {
         println!("=== Starting episode {} ===", episode);
         results.set_episode(episode);
-        run_episode(experiment, &mut results);
+        run_episode(episode, experiment, &mut results, &mut rng);
     }
 
     results
 }
 
-fn run_episode<'a>(experiment: &'a ExperimentConfig, results: &mut ExperimentResults) {
-    for step in 0..experiment.nsteps_per_episode {
-        // TODO: Generate the next set of motor coordinates and write them to results
+fn run_episode<'a>(episode: u64, experiment: &'a ExperimentConfig, results: &mut ExperimentResults, rng: &mut rand::ThreadRng) {
+    // Starting angles
+    let mut base: isize = ANGLE_START_BASE;
+    let mut shoulder: isize = ANGLE_START_SHOULDER;
+    let mut elbow: isize = ANGLE_START_ELBOW;
+
+    // Create the script for this episode
+    let mut scriptname = String::new();
+    write!(scriptname, "tmpscript_episode_{}.txt", episode);
+    let mut f = match File::create(scriptname) {
+        Err(e) => {
+            let mut msg = String::new();
+            writeln!(msg, "Could not run episode {} due to error in opening script file: {:?}", episode, e);
+
+            write!(results, "{}", msg);
+            print!("{}", msg);
+            return;
+        },
+        Ok(file) => file,
+    };
+
+    // For each step, do a random step of up to 15 degrees in either direction on each servo
+    for _step in 0..experiment.nsteps_per_episode {
+        // Generate a bunch of values
+        let randbase = rng.gen_range(-15, 16);
+        let randshoulder = rng.gen_range(-15, 16);
+        let randelbow = rng.gen_range(-15, 16);
+
+        // Add the values to the joints
+        base += randbase;
+        shoulder += randshoulder;
+        elbow += randelbow;
+
+        // Clamp the joints to between min and max for each joint
+        base = num::clamp(base, ANGLE_LOWER_LIMIT_BASE, ANGLE_UPPER_LIMIT_BASE);
+        shoulder = num::clamp(shoulder, ANGLE_LOWER_LIMIT_SHOULDER, ANGLE_UPPER_LIMIT_SHOULDER);
+        elbow = num::clamp(elbow, ANGLE_LOWER_LIMIT_ELBOW, ANGLE_UPPER_LIMIT_ELBOW);
+
+        // Write to the file
+        writeln!(f, "servo {} {}", BASENUM, base);
+        writeln!(f, "servo {} {}", SHOULDERNUM, shoulder);
+        writeln!(f, "servo {} {}", ELBOWNUM, elbow);
+
+        // Also write to results
+        writeln!(results, "servo {} {}", BASENUM, base);
+        writeln!(results, "servo {} {}", SHOULDERNUM, shoulder);
+        writeln!(results, "servo {} {}", ELBOWNUM, elbow);
     }
+
+    // Make sure to go to home after every episode and spend a few cycles there.
+    writeln!(f, "home");
+    writeln!(f, "home");
+    writeln!(f, "home");
+
+    // Execute the script
+    let mut cmd = if cfg!(target_os = "windows") {
+        process::Command::new("target/debug/teleop.exe")
+    } else {
+        process::Command::new("target/debug/teleop")
+    };
+    let s = match cmd.status() {
+        Ok(status) => {
+            writeln!(results, "Executed episode {} successfully. Exit status: {}", episode, status);
+            status
+        },
+        Err(e) => {
+            writeln!(results, "Could not execute episode {}. Error: {:?}", episode, e);
+            return;
+        },
+    };
+    match s.code() {
+        Some(0) => writeln!(results, "Script executed successfully.").unwrap(),
+        Some(v) => writeln!(results, "Script exited abnormally with exit status {}", v).unwrap(),
+        None => (),
+    };
 }
